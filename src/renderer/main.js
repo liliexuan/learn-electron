9efece29551b746ec20d2ac44a52286a7c23c5c5
liliexuan <liliexuan@163.com>
2018-02-09 18:06:40 +0800

    element-ui

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
-
 import App from './App'
 import router from './router'
 import store from './store'
@@ -9,6 +10,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
